bf127fbe44c53793711b29e59ad73eb43de837c7
Alexander Freyr <alexander-freyr.torgeirsson.3043@student.uu.se>
2024-01-30 14:18:50 +0100

    Various bug fixes

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,27 +1,26 @@
 using UnityEngine;
 using UnityEngine.Events;
 
+[RequireComponent(typeof(Rigidbody2D)), DisallowMultipleComponent]
 public class Health : MonoBehaviour {
 
-    public UnityEvent onHit;
-    public UnityEvent onDeath;
     public int health = 1;
+    public UnityEvent onHit, onDeath;
 
-    void OnTriggerEnter(Collider other) 
+    void OnTriggerEnter2D(Collider2D collision)
     {
         // If the other object has a Damage component
         // use a copy of that component to get the damage value
-        if (other.TryGetComponent<Damage>(out Damage damageComponent))
+        if (collision.TryGetComponent(out Damage damageComponent))
         {
-            Debug.Log(name + " hit by " + other.name);
-            onHit.Invoke();
             health -= damageComponent.damage;
+            onHit?.Invoke();
 
             if (health <= 0)
-            {
-                onDeath.Invoke();
-                Destroy(gameObject);
-            }
+                onDeath?.Invoke();
         }
     }
+
+    public void Destroy() => Destroy(gameObject);
+    public void DestroyRoot() => Destroy(transform.root.gameObject);
 }
