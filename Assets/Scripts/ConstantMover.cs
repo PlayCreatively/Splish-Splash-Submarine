2abf35d4304467bc3ed5fce65482856e298924aa
Alexander Freyr <alexander-freyr.torgeirsson.3043@student.uu.se>
2024-01-30 15:50:24 +0100

    Enemies move down
    + ConstantMover.cs

---
+++ Assets/Scripts/ConstantMover.cs
@@ -0,0 +1,12 @@
+﻿
+using UnityEngine;
+
+class ConstantMover: MonoBehaviour
+{
+    public Vector2 direction;
+
+    void Update()
+    {
+        transform.position += (Vector3)(Time.deltaTime * direction);
+    }
+}
