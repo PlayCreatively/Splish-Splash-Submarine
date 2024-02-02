1592ffe2024fcd4a1c4bb556d399e0e9e498b9c7
Alexander Freyr <alexander-freyr.torgeirsson.3043@student.uu.se>
2024-02-02 14:50:41 +0100

    Shooting enhancements Radically slowed down the shooting rate and made it visible when you can shoot again.

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Shooter : MonoBehaviour
 {
     [SerializeField]
     Rigidbody2D bulletPrefab;
+    public UnityEvent onReloaded, onShot;
 
     Timer shootRate;
+    bool canShoot = false;
 
     private void Awake()
     {
@@ -15,23 +18,34 @@ public class Shooter : MonoBehaviour
 
         GlobalSettings.Current.shooting.onValidate += 
             (shooting) => shootRate.Start(shooting.shootRate);
+
+        onReloaded.AddListener(() => GetComponent<SpriteRenderer>().color = new Color(1, .6f, .6f));
+        onShot.AddListener(() => GetComponent<SpriteRenderer>().color = Color.white);
     }
 
     private void Update()
     {
-        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
-            TryShoot();
-    }
+        // check if the player can shoot
+        if (!canShoot)
+        {
+            if (shootRate)
+            {
+                canShoot = true;
+                onReloaded.Invoke();
+            }
+            else
+                return;
+        }
 
-    public void TryShoot()
-    {
-        if (shootRate)
+        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
             Shoot();
     }
 
     void Shoot()
     {
+        canShoot = false;
         shootRate.Restart();
+        onShot.Invoke();
         Rigidbody2D bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
         bullet.velocity = transform.up * GlobalSettings.Current.shooting.bulletSpeed;
 
