1410eacf16014c241407bd17c97681ebaa726100
Alexander Freyr <alexander-freyr.torgeirsson.3043@student.uu.se>
2024-02-06 00:15:03 +0100

    Removed TMP

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -1,13 +1,14 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Score : MonoBehaviour
 {
     private float score = 0;
-    // Update is called once per frame
+
     void Update()
     {
-        this.GetComponent<TextMeshProUGUI>().text = "Score: " + ((int)score).ToString();
-        score += Time.deltaTime * 10;
+        GetComponent<Text>().text = ((int)score).ToString();
+        score += Time.deltaTime;
     }
 }
