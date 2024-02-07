58871be9c5f5fe4b5d87cd4817c9424b6bbdf1a8
Alexander Freyr <alexander-freyr.torgeirsson.3043@student.uu.se>
2024-02-07 12:26:41 +0100

    Implemented a mode switcher
    + ModeSwitcher.cs
    
    Press tab to switch modes. NB, this feature is disabled in the editor.

---
+++ Assets/ModeSwitcher.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ModeSwitcher : MonoBehaviour
+{
+    int currentModeIndex = -1;
+    static GameSettings[] modes;
+
+    Timer fadeOut;
+    Text text;
+
+    void Awake()
+    {
+        fadeOut = new(2);
+        text = GetComponent<Text>();
+        text.enabled = true;
+        modes ??= Resources.LoadAll<GameSettings>("Settings/Game Modes");
+        currentModeIndex = Array.IndexOf(modes, GlobalSettings.Get._current);
+        text.text = modes[currentModeIndex].ToString();
+
+    }
+
+    void Update()
+    {
+        if (!Application.isEditor && Input.GetKeyDown(KeyCode.Tab) && Time.timeSinceLevelLoad > .5f)
+        {
+            LoadMode(currentModeIndex + 1);
+        }
+
+        if(!fadeOut)
+        {
+            var temp = text.color;
+            temp.a = 1f - fadeOut;
+            text.color = temp;
+        }
+    }
+
+    void LoadMode(int index)
+    {
+        currentModeIndex = index % modes.Length;
+
+        GlobalSettings.Get._current = modes[currentModeIndex];
+
+        SceneManager.LoadScene(0);
+    }
+}
