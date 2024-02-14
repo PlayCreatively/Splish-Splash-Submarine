e169044e748f6b71f8cf7973e7c63c6a391e65e0
Alexander Freyr <alexander-freyr.torgeirsson.3043@student.uu.se>
2024-02-14 12:26:59 +0100

    Removed ADSREnvelope.cs

--- Assets/Scripts/ScriptableObjects/DashingSettings.cs
+++ Assets/Scripts/ScriptableObjects/DashingSettings.cs
@@ -5,9 +5,7 @@ public class PlayerSettings : SettingsBase<PlayerSettings>
 {
     [Min(0), Header("Boost")]
     public float boostSpeed = 1;
-    public ADSREnvelope boostResponsiveness = ADSREnvelope.Default();
 
     [Min(0), Header("HorizontalMovement")]
     public float horizontalSpeed = 1;
-    public ADSREnvelope horizontalResponsiveness = ADSREnvelope.Default();
 }
