894bbce40df75f1640ceacb2d8cd3313a49cea41
Alexander Freyr <alexander-freyr.torgeirsson.3043@student.uu.se>
2024-02-17 00:56:28 +0100

    remove debug log

--- Assets/Scripts/EFBSwiper.cs
+++ Assets/Scripts/EFBSwiper.cs
@@ -34,7 +34,6 @@ public class EFBSwiper : MonoBehaviour
     {
         // Update distance to player
         GlobalSettings.Current.enemyFromBehind.curDistanceFromPlayer = Mathf.MoveTowards(GlobalSettings.Current.enemyFromBehind.curDistanceFromPlayer, 0, GlobalSettings.Current.enemyFromBehind.VerticalSpeed * Time.deltaTime);
-        Debug.Log($"enemy {GlobalSettings.Current.enemyFromBehind.VerticalSpeed} : player {GlobalSettings.Current.player.curVerticalSpeed}");
 
         float distanceToVisibility = GlobalSettings.Current.enemyFromBehind.curDistanceFromPlayer - GlobalSettings.Current.enemyFromBehind.tipPosition;
 
