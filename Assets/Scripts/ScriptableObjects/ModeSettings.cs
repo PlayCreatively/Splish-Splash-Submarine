4c467d8fe9e31395422bc6776182cdba52980c43
Alexander Freyr <alexander-freyr.torgeirsson.3043@student.uu.se>
2024-02-17 21:12:00 +0100

    Global scalar for game speed

--- Assets/Scripts/ScriptableObjects/ModeSettings.cs
+++ Assets/Scripts/ScriptableObjects/ModeSettings.cs
@@ -3,10 +3,17 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New"+nameof(ModeSettings), menuName = "Setting Objects/" + nameof(ModeSettings), order = -10)]
 public class ModeSettings : ScriptableObject
 {
+    [Range(0, 1)]
+    public float timeScale = 1;
     public PlayerSettings player;
     //public EnemySettings enemy;
     public EnemyFromBehindSettings enemyFromBehind;
     public RadarSettings radar;
     public ShootingSettings shooting;
     public SpawnerSettings spawnerSettings;
+
+    void OnValidate()
+    {
+        Time.timeScale = timeScale;
+    }
 }
