bfb6ae64db5fa7b176fdc2b38aa4aecc45576b25
Alexander Freyr <alexander-freyr.torgeirsson.3043@student.uu.se>
2024-02-21 14:22:39 +0100

    Player settings and component cleanup

--- Assets/Scripts/ScriptableObjects/PlayerSettings.cs
+++ Assets/Scripts/ScriptableObjects/PlayerSettings.cs
@@ -3,24 +3,15 @@
 [CreateAssetMenu(fileName = "New" + nameof(PlayerSettings), menuName = Path + nameof(PlayerSettings), order = 0)]
 public class PlayerSettings : SettingsBase<PlayerSettings>
 {
-    public NormalCurve dashCurve;
     [Min(0)]
-    public float dashSpeed = 1;
-    [Min(0)]
-    public float dashDuration = 1;
-    
-    public NormalCurve collisionRecoverCurve;
-    [Min(0), Tooltip("How long it takes the player to recover from colliding with a fish")]
-    public float collisionRecoverTime = 1;
+    public float verticalSpeed = 1;
 
-    [Min(0)] public float 
-        horizontalSpeed = 1,
-        verticalSpeed = 1;
+    public NormalCurve horizontalCurve;
+    [Min(0)] 
+    public float horizontalSpeed = 1;
 
     [HideInInspector]
     public float curVerticalSpeed;
-    [HideInInspector]
-    public bool recoveringFromCollision;
 
     public  Transform Ref => _player != null 
         ? _player 
