b9c5d0321d130d3ae77e0ce51f9193c110f09c52
Alexander Freyr <alexander-freyr.torgeirsson.3043@student.uu.se>
2024-02-21 18:35:44 +0100

    Mode settings clean-up

--- Assets/Scripts/ScriptableObjects/EnemyFromBehindSettings.cs
+++ Assets/Scripts/ScriptableObjects/EnemyFromBehindSettings.cs
@@ -5,9 +5,6 @@ public class EnemyFromBehindSettings : SettingsBase<EnemyFromBehindSettings>
 {
     [HideInInspector]
     public float curMoveSpeedOverPlayer = 0;
-    [Min(0)]
-    public float swipeDuration = 1;
-
     [HideInInspector]
     public float curDistanceFromPlayer = 1;
     public float maxDistanceFromPlayer = 5;
