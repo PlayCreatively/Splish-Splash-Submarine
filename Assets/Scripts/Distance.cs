e4b8bbb5ded2d28110a0b7d36f43d450a79dbdf3
Joel Nataniel Segersam <joel.segersam.0807@student.uu.se>
2024-02-25 21:59:56 +0100

    Distance stops when caught
    +Bullet note in right place in hierarchy

--- Assets/Scripts/Distance.cs
+++ Assets/Scripts/Distance.cs
@@ -6,21 +6,26 @@ public class Distance : MonoBehaviour
 {
     // distance in "targets" reached
     float distance = 0;
+    bool isMoving = true;
     const int targetInMeters = 100;
     public int timeToTarget = 30;
     public Text distanceText;
     public Transform preassurePin;
     public float prsPinRelSpeed = 0.4f;
+    public EFBLurker efbLurker;
 
     void Start()
     {
         transform.localScale = new Vector3(1, 0, 1);
+
+        efbLurker.OnCaught.AddListener(() => isMoving = false);
     }
 
     void Update()
     {
-        // Increase the distance by 1 every timeToTarget seconds
-        distance += Time.deltaTime / timeToTarget;
+        if (isMoving)
+            // Increase the distance by 1 every timeToTarget seconds
+            distance += Time.deltaTime / timeToTarget;
 
         transform.localScale = new Vector3(1, distance % 1, 1);
 
