22c76847ac071bb9c04fcb47efd1c491d8a04be1
Alexander Freyr <alexander-freyr.torgeirsson.3043@student.uu.se>
2024-03-02 15:45:23 +0100

    Bullet minimize & frame freeze

---
+++ Assets/Scripts/Utility Scripts/FrameFreeze.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrameFreeze : MonoBehaviour
+{
+    [SerializeField]
+    NormalCurve freezeCurve;
+
+
+    public void Freeze(float duration)
+    {
+        StartCoroutine(FreezeFrame(duration));
+    }
+
+    private IEnumerator FreezeFrame(float duration)
+    {
+        float freezeTimer = 0;
+        float normal = 0;
+        float previousTimeScale = Time.timeScale;
+        
+        while (normal < 1f)
+        {
+            yield return null;
+            freezeTimer += Time.unscaledDeltaTime;
+            normal = freezeTimer / duration;
+
+            Time.timeScale = previousTimeScale * (1f - freezeCurve.Evaluate(normal));
+        }
+    }
+}
