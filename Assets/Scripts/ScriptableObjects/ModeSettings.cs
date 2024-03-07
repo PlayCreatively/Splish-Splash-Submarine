b136bedf1ee5a03f3e2a9b0ef610dc19c1565d68
Alexander Freyr <alexander-freyr.torgeirsson.3043@student.uu.se>
2024-03-07 01:56:35 +0100

    bug fixes

--- Assets/Scripts/ScriptableObjects/ModeSettings.cs
+++ Assets/Scripts/ScriptableObjects/ModeSettings.cs
@@ -7,11 +7,17 @@ public class ModeSettings : ScriptableObject
     public float timeScale = 1;
     [SerializeField, Min(0)]
     float levelLengthInMinutes = 1;
-    public float LevelLength => levelLengthInMinutes * 60f * player.verticalSpeed * Time.timeScale;
+    [HideInInspector]
+    public float LevelLength;
     public PlayerSettings player;
     //public EnemySettings enemy;
     public EnemyFromBehindSettings enemyFromBehind;
     public RadarSettings radar;
     public ShootingSettings shooting;
     public SpawnerSettings spawnerSettings;
+
+    private void OnEnable()
+    {
+        LevelLength = levelLengthInMinutes * 60f * player.verticalSpeed * Time.timeScale;
+    }
 }
