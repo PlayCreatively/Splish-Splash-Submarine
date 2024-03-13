b1c828592fda8e0f6e1c621a718c7482cfd2b89d
tundra_moss <aleksandra.pavlova-verevkina.1376@student.uu.se>
2024-03-13 22:11:13 +0100

    Update TitleScreenMusic.cs
    Put main menu as default for starting the game + removed comments from TitleScreenMusic.cs

--- Assets/Scripts/Utility Scripts/TitleScreenMusic.cs
+++ Assets/Scripts/Utility Scripts/TitleScreenMusic.cs
@@ -4,19 +4,16 @@ using UnityEngine;
 
 public class TitleScreenMusic : MonoBehaviour
 {
-    //public AudioClip musicClip;
     private AudioSource audioSource;
     public GameObject creditsMenu;
 
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
-        //audioSource.clip = musicClip;
     }
 
     void Update()
     {
-        // Example condition to mute the music (replace with your own condition)
         if (creditsMenu.activeSelf)
         {
             audioSource.mute = true;
