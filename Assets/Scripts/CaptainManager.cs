99eb0e691dc25131205f78a8cebdadc781422deb
Joel Nataniel Segersam <joel.segersam.0807@student.uu.se>
2024-03-13 17:24:06 +0100

    CaptainManager clean up

--- Assets/Scripts/CaptainManager.cs
+++ Assets/Scripts/CaptainManager.cs
@@ -6,14 +6,8 @@ public class CaptainManager : MonoBehaviour
     //SwitchText switchText;
     public Text captainText;
 
-    void Start()
-    {
-        //switchText = captainText.gameObject.GetComponent<SwitchText>();
-    }
-
     public void Say(string line)
     {
         captainText.text = line;
-        Debug.Log("Changed to " + line);
     }
 }
